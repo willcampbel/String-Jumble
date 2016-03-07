3751abe61dc8d8610c7915bb60233f66d3f3aed6
willcampbel <williamcampbell@hanovernorwichschools.org>
2016-03-07 14:39:07 -0500

    Updated from Brython Server: 3/7/2016 2:39:13 PM

--- stringjumble.py
+++ stringjumble.py
@@ -25,3 +25,9 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+string1 = input("PLease enter a string of text (the bigger the better): ")
+print('You entered "'+string+'")
+
+
+string1=list(string1)
+print(string1)
